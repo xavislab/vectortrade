cd720420d87b598195a77bc9522d911e6c6f01b4
xavislab <3.22333303+xavislab@users.noreply.github.com>
2026-09-01 03:32:02 +0000

    Fix Vercel auth function imports

--- api/trpc/[...path].ts
+++ api/trpc/[...path].ts
@@ -1,12 +1,16 @@
+import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { appRouter } from "../server/routers";
-import { createContext } from "../server/_core/context";
+import { appRouter } from "../../server/routers";
+import { createContext } from "../../server/_core/context";
 
-const middleware = createExpressMiddleware({
-  router: appRouter,
-  createContext,
-});
+const app = express();
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
 
-export default function handler(req: any, res: any) {
-  return middleware(req, res, () => undefined);
-}
+export default app;
